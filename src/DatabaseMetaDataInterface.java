47c8f3a1c546813143aa00349a754869497bc319
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-07-24 14:27:25 +0530

    DatabaseMetaDataInterface.java

---
+++ src/DatabaseMetaDataInterface.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class DatabaseMetaDataInterface {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java-jdbc", "root", "");
+
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+            System.out.println("Driver Name: " + databaseMetaData.getDriverName());
+            System.out.println("Driver Version: " + databaseMetaData.getDriverVersion());
+            System.out.println("UserName: " + databaseMetaData.getUserName());
+            System.out.println("Database Product Name: " + databaseMetaData.getDatabaseProductName());
+            System.out.println("Database Product Version: " + databaseMetaData.getDatabaseProductVersion());
+
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
