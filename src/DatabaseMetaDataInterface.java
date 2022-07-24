661806784476329caeadd43e749ea271eb952299
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-07-24 14:31:38 +0530

    DatabaseMetaDataInterface.java get tables

--- src/DatabaseMetaDataInterface.java
+++ src/DatabaseMetaDataInterface.java
@@ -14,6 +14,14 @@ public class DatabaseMetaDataInterface {
             System.out.println("Database Product Name: " + databaseMetaData.getDatabaseProductName());
             System.out.println("Database Product Version: " + databaseMetaData.getDatabaseProductVersion());
 
+            System.out.println("Database Tables:");
+            String table[]={"TABLE"}; //For view make TABLE as VIEW
+            ResultSet resultSet=databaseMetaData.getTables(null,null,null,table);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(3));
+            }
+
+
             connection.close();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
